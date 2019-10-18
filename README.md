bdcf59751a7f36bfd2ffcb51ca693573ce68525e
bingliCheng <33173887+bingliCheng@users.noreply.github.com>
2019-10-18 23:33:14 +0800

    生成本地存储
    生成本地存储

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Keepname
+Export your application name.
+通过访问windows里的注册表，导出你电脑上安装的软件名。
 
 ```bash
+
+git https://github.com/bingliCheng/keepname.git
 
 cd keepname
 
--- main.js
+++ main.js
@@ -1,17 +1,45 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
+const fs = require('fs')
+
 const regedit = require('regedit')
 const regeditPath = 'HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\'
+const filePath = './database.json'
 
 let mainWindow
 
-let regArr = []
+// 最终注册表里的数据汇集的地方
+let regData = []
 
 ipcMain.on('getRegedit', (event, arg) => {
-  event.sender.send('getRegedit-reply', regArr)
+
+  fs.exists(path.join(__dirname, filePath), (exists) => {
+    if (exists) { // 如果已经读取过注册表就不再读取，以免造成二次堵塞进程
+      
+    } 
+    else { // 如果是第一次进来则生成文件
+      console.log('writting file...')
+      let tmpText = `{"data" : [`
+      
+      for (let item in regData) {
+        tmpText += '"' + regData[item] + '",'
+      }
+      tmpText = tmpText.substr(0, tmpText.length - 1) //删除拼接后的最后一个逗号
+      tmpText += `]}`
+
+      fs.writeFile(filePath, tmpText, err => {
+        if (err) {
+          console.log(`err: ${err}`)
+        }
+        console.log('Write successfully ')
+      })
+    }
+  })
+
+  event.sender.send('getRegedit-reply', regData)
 })
 
-function getRegedit(cb) {
+function getRegedit() {
   // 开始获取注册表
   regedit.list([regeditPath], function (err, data) {
     if (err) {
@@ -47,7 +75,7 @@ function getRegedit(cb) {
             // 首先，数据数量必须大于5，过滤无效注册表
             // 其次，我们要取的程序名字不能为空，也是防止报错
             if (Object.keys(tmpValues).length >= 5 && tmpValues['DisplayName'] !== undefined) {
-              regArr.push(tmpValues['DisplayName']['value'])
+              regData.push(tmpValues['DisplayName']['value'])
             }
           }
         })
@@ -69,7 +97,11 @@ function createWindow() {
 
   mainWindow.loadFile('index.html')
 
-  getRegedit()
+  fs.exists(path.join(__dirname, filePath), (exists) => { // 如果已经读取过注册表就不再读取，以免造成二次堵塞进程
+    if (!exists) getRegedit()
+  })
+
+  mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', function () {
     mainWindow = null
