fd7d3e9064fe3da073e8bea5d60be38dfafed7bd
bingliCheng <33173887+bingliCheng@users.noreply.github.com>
2019-10-20 21:51:19 +0800

    添加分号
    添加分号

--- js/index.js
+++ js/index.js
@@ -1,79 +1,78 @@
 /* Index Javascript */
-const $readRegeditBtn = $('#readRegeditBtn')
-const $displayBtn = $('#displayBtn')
-const $reloadBtn = $('#reloadBtn')
+const $readRegeditBtn = $('#readRegeditBtn');
+const $displayBtn = $('#displayBtn');
+const $reloadBtn = $('#reloadBtn');
 
+$displayBtn.attr('disabled', 'true');
 
-$displayBtn.attr('disabled', 'true')
-
-const $regedit = $('#readReditBtn')
-const $tips = $('.tips')
-const $tbody = $('.tbody')
-const $count = $tips.find('.count')
+const $regedit = $('#readReditBtn');
+const $tips = $('.tips');
+const $tbody = $('.tbody');
+const $count = $tips.find('.count');
 
 function replaceTipsHTML(content) {
-  $tips.find('.tips-left').html(content)
+  $tips.find('.tips-left').html(content);
 }
 
-let countNum = parseInt($count.text()) + 1
+let countNum = parseInt($count.text()) + 1;
 
 function addClickCount() { // 增加按钮点击数
-  $count.text(countNum++)
+  $count.text(countNum++);
 }
 
 function cloneTemplate(parent, index, data) { // 克隆模板
-  let template = $('.template').clone()
-  template.children('.num').html(index)
-  template.children('.product').html(data)
-  template.removeClass('template').addClass('block')
-  parent.append(template)
+  let template = $('.template').clone();
+  template.children('.num').html(index);
+  template.children('.product').html(data);
+  template.removeClass('template').addClass('block');
+  parent.append(template);
 }
 
-let tmpData
+let tmpData;
 
 function setTempData(data) { // 临时存储所有数据在当前全局变量（游览器内存）
-  tmpData = data
+  tmpData = data;
 }
 
 $readRegeditBtn.on('click', () => {
-  $displayBtn.removeAttr('disabled')
-  addClickCount()
-  ipcRenderer.send('getRegedit', 'ping')
+  $displayBtn.removeAttr('disabled');
+  addClickCount();
+  ipcRenderer.send('getRegedit', 'ping');
 })
 
 
 ipcRenderer.on('getRegedit-reply', (event, arg) => {
-  setTempData(arg)
+  setTempData(arg);
 
-  replaceTipsHTML(`已经成功读取到注册表数据，请点击显示信息按钮。`)
+  replaceTipsHTML(`已经成功读取到注册表数据，请点击显示信息按钮。`);
   
 })
 
 $displayBtn.on('click', () => {
-  addClickCount()
-  ipcRenderer.send('readRegedit', 'ping')
+  addClickCount();
+  ipcRenderer.send('readRegedit', 'ping');
 })
 
 ipcRenderer.on('readRegedit-reply', (event, arg) => {
-  replaceTipsHTML(`本次已读取到${tmpData.length}个软件名字。`)
+  replaceTipsHTML(`本次已读取到${tmpData.length}个软件名字。`);
 
-  $tbody.children('tr.block').remove()
+  $tbody.children('tr.block').remove();
 
   for (let i = 0; i < tmpData.length; i++) { // 生成table
-    cloneTemplate($tbody, (i + 1), tmpData[i])
+    cloneTemplate($tbody, (i + 1), tmpData[i]);
   }
 })
 
 $reloadBtn.on('click', () => {
-  addClickCount()
-  ipcRenderer.send('reloadRegedit', 'ping')
+  addClickCount();
+  ipcRenderer.send('reloadRegedit', 'ping');
 })
 
 $('.fix-bar span.glyphicon').on('click', function () {
-  let $this = $(this)
+  let $this = $(this);
 
   if ($this.hasClass('glyphicon-arrow-up'))
-    $('html, body').animate({ scrollTop: '0px' }, 500)
+    $('html, body').animate({ scrollTop: '0px' }, 500);
   else
-    $('html, body').animate({ scrollTop: $(document).height() + 'px' }, 500)
+    $('html, body').animate({ scrollTop: $(document).height() + 'px' }, 500);
 })
--- main.js
+++ main.js
@@ -1,123 +1,115 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
-const fs = require('fs')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const fs = require('fs');
 
-const regedit = require('regedit')
-const regeditPath = 'HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\'
-const filePath = './database.json'
+const regedit = require('regedit');
+const regeditPath = 'HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\';
+const filePath = './database.json';
 
-let mainWindow
+let mainWindow;
 
 // 最终注册表里的数据汇集的地方
-let regData = []
+let regData = [];
 
 ipcMain.on('getRegedit', (event, arg) => {
   fs.exists(path.join(__dirname, filePath), (exists) => { // 判断文件是否存在
     if (exists) { // 如果已经读取过注册表就不再读取，以免造成二次堵塞进程
-      PostLocalData(event, filePath)
+      PostLocalData(event, filePath);
     } 
     else { // 如果是第一次进来则生成文件
-      console.log('writting file...')
+      console.log('writting file...');
 
-      let tmpText
+      let tmpText;
 
-      tmpText = dealText(regData)
+      tmpText = dealText(regData);
 
-      writeFile(filePath, tmpText)
-      event.sender.send('getRegedit-reply', regData)
+      writeFile(filePath, tmpText);
+      event.sender.send('getRegedit-reply', regData);
     }
-  })
-})
+  });
+});
 
 ipcMain.on('readRegedit', (event, arg) => {
-  event.sender.send('readRegedit-reply', regData)
-})
+  event.sender.send('readRegedit-reply', regData);
+});
 
 ipcMain.on('reloadRegedit', (event, arg) => {
   getRegedit(() => {
-    console.log(123)
-  })
-  // let tmpText
-
-  // console.log(regData)
-
-  // tmpText = dealText(regData)
-
-  // writeFile(filePath, tmpText)
-})
+    console.log(regData);
+  });
+});
 
 function dealText(data) { // 拼接JSON数据
-  let tmpText = `{"data" : [`
+  let tmpText = `{"data" : [`;
   for (let item in data) {
-    tmpText += '"' + data[item] + '",'
+    tmpText += '"' + data[item] + '",';
   }
-  tmpText = tmpText.substr(0, tmpText.length - 1) //删除拼接后的最后一个逗号
-  tmpText += `]}`
+  tmpText = tmpText.substr(0, tmpText.length - 1); //删除拼接后的最后一个逗号
+  tmpText += `]}`;
 
-  return tmpText
+  return tmpText;
 }
 
 function writeFile(path, content) { // 写入数据到本地文件
   fs.writeFile(path, content, err => {
     if (err) {
-      console.log(`err: ${err}`)
+      console.log(`err: ${err}`);
     }
-    console.log('Write successfully ')
-  })
+    console.log('Write successfully ');
+  });
 }
 
 function PostLocalData(event, path) {
   fs.readFile(path, (err, data) => { // 读取本地json
-    if (err) return console.log(err)
+    if (err) return console.log(err);
 
-    let tmpObj = JSON.parse(data)
-    event.sender.send('getRegedit-reply', tmpObj.data)
-  })
+    let tmpObj = JSON.parse(data);
+    event.sender.send('getRegedit-reply', tmpObj.data);
+  });
 }
 
-function getRegedit(cb) {
-  // 开始获取注册表
+function getRegedit(cb) { // 开始获取注册表
+  
   regedit.list([regeditPath], function (err, data) {
     if (err) {
-      console.log('err' + err)
+      console.log('err' + err);
     }
     // 遍历目录
     for (let item in data) {
       // data[item].keys.length 长度
-      let keys = data[item].keys
+      let keys = data[item].keys;
       // 获取相对应的项目名字
       // keys.length
       for (let i = 0; i <= keys.length; i++) {
-        let keyName = keys[i]
+        let keyName = keys[i];
         // console.log(keys[i])
-
-        // 防止空项目报错
-        if (keyName === undefined) {
-          break
+        
+        if (keyName === undefined) { // 防止空项目报错
+          break;
         }
 
         regedit.list([regeditPath + keyName], function (err, data) {
           if (err) {
-            console.log('err' + err)
+            console.log('err' + err);
           }
           // 将当前数据存放到数组
-          let tmpArr = []
+          let tmpArr = [];
           for (let i in data) {
-            tmpArr.push(data[i])
+            tmpArr.push(data[i]);
           }
 
-          let tmpValues = tmpArr[0].values
+          let tmpValues = tmpArr[0].values;
           if (tmpValues !== undefined) {
             // 首先，数据数量必须大于5，过滤无效注册表
             // 其次，我们要取的程序名字不能为空，也是防止报错
             if (Object.keys(tmpValues).length >= 5 && tmpValues['DisplayName'] !== undefined) {
-              regData.push(tmpValues['DisplayName']['value'])
+              regData.push(tmpValues['DisplayName']['value']);
             }
           }
+          cb && cb();
         })
       }
     }
-    cb()
   })
 }
 
@@ -132,25 +124,25 @@ function createWindow() {
     }
   })
 
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
   fs.exists(path.join(__dirname, filePath), (exists) => { // 如果已经读取过注册表就不再读取，以免造成二次堵塞进程
-    if (!exists) getRegedit()
-  })
+    if (!exists) getRegedit();
+  });
 
   mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', function () {
-    mainWindow = null
+    mainWindow = null;
   })
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit();
 })
 
 app.on('activate', function () {
-  if (mainWindow === null) createWindow()
+  if (mainWindow === null) createWindow();
 })
